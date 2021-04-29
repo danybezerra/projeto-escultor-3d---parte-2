48bdf1a4d663d72a13942ada2fcc0eebb9c31a75
daniely bezerra de oliveira <57113761+danybezerra@users.noreply.github.com>
2021-04-29 08:36:35 -0300

    Update cutbox.cpp

--- cutbox.cpp
+++ cutbox.cpp
@@ -3,14 +3,14 @@
 #include "cutbox.h"
 
 cutBox::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
-    x0=x0;
-    x1=x1;
-    y0=y0;
-    y1=y1;
-    z0=z0;
-    z1=z1;
+    this->x0=x0;
+    this->x1=x1;
+    this->y0=y0;
+    this->y1=y1;
+    this->z0=z0;
+    this->z1=z1;
 };
 
 void cutBox::draw(sculptor &t){
-    t.cutBox(x0, x1, y0, y1, z0, z1);
+    t.cutBox(this->x0, this->x1, this->y0, this->y1, this->z0, ths->z1);
 };
