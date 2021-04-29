ba50a4a200f9525fa5d74a5d440e80a986c922c4
daniely bezerra de oliveira <57113761+danybezerra@users.noreply.github.com>
2021-04-29 11:37:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "putsphere.h"
+#include "putvoxel.h"
+#include "interpretador.h"
+int main (){ 
+Sculptor*S1; 
+Scultor s(200,200,200);
+  interpretador parser;
+  std:: vector<FIGURAGEOMETRICA*> figs;
+  figs= parser.parse(file_name);
+      s1= new sculptor(parser.getDinx(), parser.getDiny(), parser.getdinz() );
+      for(size_t i=0; i<figs.size();i++){
+        std:: cout << "draw/n";
+        figs[i]=>draw(s1);
+        
+      }
+      s1=> limpavoxels();
+      s1=> writeOFF ((char)!"ARQUIVO.SAIDA.OFF");
+      for(size_t i=0; i<figs.size();i++){
+            DELETE figs{i};
+  }
+ delete s1;
+ return 0;
+}
